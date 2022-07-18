862f67f22cc7357e02f27d3fbf59197cce5c9ae0
Taylor Fraley <tfraley@clearent.com>
2022-07-18 16:52:00 -0400

    some examples from brainstorming

---
+++ examples/poll-queues-result.md
@@ -0,0 +1,3 @@
+Queue                        Ready   Unacked   Total 
+CompassUserChangeQueue           0         1       1 
+CompassUserChangeQueue_error     1         0       1 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut file = File::open(&args[1])?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!();
+    println!("{contents}");
+    println!("Press ENTER to stop");
+    let mut buffer = String::new();
+    let stdin = io::stdin(); // We get `Stdin` here.
+    stdin.read_line(&mut buffer)?;
+    Ok(())
 }
